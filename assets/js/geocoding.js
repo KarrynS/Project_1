290c7ea5bc486eef6984555cc887d3d11a96a7a6
Nathan Opresnik <nopresnik@me.com>
2020-12-21 10:57:10 +1100

    Complete getCoords function

--- assets/js/geocoding.js
+++ assets/js/geocoding.js
@@ -1,9 +1,9 @@
-function getCoordsFromLocation(location) {
+function getCoordsFromLocation(location, cb) {
   const settings = {
     async: true,
     crossDomain: true,
     url:
-      "https://forward-reverse-geocoding.p.rapidapi.com/v1/search?q=Shepparton%20Victoria&format=json&accept-language=en&limit=1&polygon_threshold=0.0",
+      `https://forward-reverse-geocoding.p.rapidapi.com/v1/search?q=${location}&format=json&accept-language=en&limit=1&polygon_threshold=0.0`,
     method: "GET",
     headers: {
       "x-rapidapi-key": "ixAie5Ghfgmsh9LoyF33FoV3i8jkp1u32SjjsnGIH86iebObCj",
@@ -12,6 +12,11 @@ function getCoordsFromLocation(location) {
   };
 
   $.ajax(settings).done(function (response) {
-    console.log(response);
+    var result = {
+      displayName: response[0].display_name,
+      lat: response[0].lat,
+      lon: response[0].lon,
+    }
+    if (cb) cb(result);
   });
 }
