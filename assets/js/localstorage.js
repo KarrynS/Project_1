cb795468cb8b092ab1a62b24621b56eafccb32b8
Phoenix <73591532+KarrynS@users.noreply.github.com>
2021-01-04 19:00:11 +1100

    Updating rendersearch on page load

--- assets/js/localstorage.js
+++ assets/js/localstorage.js
@@ -2,31 +2,32 @@
 var savedCuisines = [];
 
 //Rendering Search Function and adding previous search to list
-function renderSearch () {
-    $("#searchButton").on("click", function(event) {
-    event.preventDefault();
-    var cuisineSearch = $("#findtext").val().trim();
-    console.log(cuisineSearch);
+$("#searchButton").on("click", function (event) {
+  event.preventDefault();
+  renderSearch();
+})
+function renderSearch() {
+  var cuisineSearch = $("#findtext").val().trim();
+  console.log(cuisineSearch);
 
-    //Retrieving from local storage and updating search result
-    savedCuisines = localStorage.getItem("savedCuisines");
-    savedCuisines = JSON.parse(savedCuisines);
-    if ( !savedCuisines ){
-      savedCuisines = [];
-    }
-    savedCuisines.push(cuisineSearch);
+  //Retrieving from local storage and updating search result
+  savedCuisines = localStorage.getItem("savedCuisines");
+  savedCuisines = JSON.parse(savedCuisines);
+  if (!savedCuisines) {
+    savedCuisines = [];
+  }
+  savedCuisines.push(cuisineSearch);
 
-    //Saving previous searches to local storage
-    localStorage.setItem("savedCuisines", JSON.stringify(savedCuisines));
+  //Saving previous searches to local storage
+  localStorage.setItem("savedCuisines", JSON.stringify(savedCuisines));
 
-     //Appending previous searches to sidebar
-     $("#offCanvasNestedOverlap").empty();
-     for ( i = 0; i < savedCuisines.length; i++) {
-       var prevCuisine = $("<div class='prevCuisine'></div");
-       prevCuisine.text(savedCuisines[i]).attr("data-name", savedCuisines[i]);
-       console.log(savedCuisines[i])
-       $("#offCanvasNestedOverlap").prepend(prevCuisine);
-       } 
-    })
+  //Appending previous searches to sidebar
+  $("#offCanvasNestedOverlap").empty();
+  for (i = 0; i < savedCuisines.length; i++) {
+    var prevCuisine = $("<div class='prevCuisine'></div");
+    prevCuisine.text(savedCuisines[i]).attr("data-name", savedCuisines[i]);
+    console.log(savedCuisines[i])
+    $("#offCanvasNestedOverlap").prepend(prevCuisine);
+  }
 }
 renderSearch();
